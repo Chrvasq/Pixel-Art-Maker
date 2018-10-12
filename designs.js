aa6440934f14209edd7cba1ff5eca61f30a0104b
Chris Vasquez <chrvasq@outlook.com>
2018-10-11 21:57:44 -0700

    Add eventListener for Submit button

--- designs.js
+++ designs.js
@@ -1,10 +1,18 @@
 // Select color input
+const colorChoice = document.querySelector("#colorPicker");
+
 // Select size input
+const gridHeightInput = document.querySelector("#inputHeight");
+const gridWidthInput = document.querySelector("#inputWidth");
 
 // When size is submitted by the user, call makeGrid()
+document.querySelector("#sizePicker").addEventListener("submit", makeGrid);
 
-function makeGrid() {
-
-// Your code goes here!
+function makeGrid(event) {
+    event.preventDefault();
 
 }
+
+console.log('grid height is ' + gridHeight);
+console.log('grid width is ' + gridWidth);
+console.log('color choice is ' + colorChoice);
